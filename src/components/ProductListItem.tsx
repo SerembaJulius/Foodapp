02ecad243dfea25d41c8f2c6efd8045f0f49d114
Seremba Julius <serembajulius@gmail.com>
2025-02-26 14:47:38 +0300

    first

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -15,7 +15,8 @@ type ProductListItemProps={
 //custom component
 const ProductListItem =({product}: ProductListItemProps)=>{
   return(
-    <Link href={'/${product.id}'} asChild>
+    // Link href={'/(tabs)/menu/[id]'} asChild>
+    <Link href={'/menu/${product.id}'} asChild>
       <Pressable style={styles.container}>
         <Image source={{uri: product.image || defaultPizzaImage }} style={styles.image} resizeMode="contain"/>
         <Text style={styles.title}>{product.name}</Text>
